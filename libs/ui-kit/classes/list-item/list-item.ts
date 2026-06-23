27209502259d3227ab49c4e7b7d989d34266b17f
Michael Sinner <michael.sinner@4soft.de>
2026-06-23 09:25:40 +0200

    chore(deps): update angular and nx to 21.2.x
    Bumps Angular, Angular CLI, Nx and related toolchain packages, and
    applies the lint/config fixups required by the upgrade (renamed
    @typescript-eslint/ban-types rule, nx.json input changes, babel
    presets for libs using SWC-less transforms, applied pending nx
    migrations).

--- libs/ui-kit/classes/list-item/list-item.ts
+++ libs/ui-kit/classes/list-item/list-item.ts
@@ -1,12 +1,12 @@
 import { Highlightable } from '@angular/cdk/a11y';
 import { ElementRef } from '@angular/core';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface,@typescript-eslint/no-unsafe-declaration-merging
+// eslint-disable-next-line @typescript-eslint/no-empty-interface,@typescript-eslint/no-empty-object-type,@typescript-eslint/no-unsafe-declaration-merging
 export interface NgDocListItem extends Highlightable {}
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export abstract class NgDocListItem implements Highlightable {
-	abstract elementRef: ElementRef<HTMLElement> | HTMLElement;
+  abstract elementRef: ElementRef<HTMLElement> | HTMLElement;
 
-	abstract selectByUser(): void;
+  abstract selectByUser(): void;
 }
--- libs/ui-kit/classes/overlay-host/overlay-host.ts
+++ libs/ui-kit/classes/overlay-host/overlay-host.ts
@@ -1,6 +1,6 @@
 import { NgDocOverlayProperties } from '@ng-doc/ui-kit/interfaces';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface,@typescript-eslint/no-unsafe-declaration-merging
+// eslint-disable-next-line @typescript-eslint/no-empty-interface,@typescript-eslint/no-empty-object-type,@typescript-eslint/no-unsafe-declaration-merging
 export interface NgDocOverlayHost extends NgDocOverlayProperties {}
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
